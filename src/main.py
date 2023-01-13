187216ae2545572e8d0de21e49cb765031feed7d
RPraneeth05 <praneeth.rangarajan.github@gmail.com>
2023-01-13 09:03:15 -0500

    Update code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import pygame
+import sys
+
+background_color = 0, 0, 0
+
+screen_size = width, height = 500, 500
+screen = pygame.display.set_mode(screen_size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.fill(background_color)
+    pygame.display.flip()
