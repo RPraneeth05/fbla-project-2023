97931205be1ac61a774aec7fd4db0e1d4c4a2ca9
Srithwak <60199432+Srithwak@users.noreply.github.com>
2023-01-26 19:54:43 -0500

    added the letter s

--- ACTUAL_SNAKE.py
+++ ACTUAL_SNAKE.py
@@ -1,5 +1,6 @@
 import pygame
 from random import randrange
+
 WINDOW = 640
 TILE_SIZE = 32
 difficulty = 0
@@ -8,15 +9,21 @@ if difficulty == 1:
 elif difficulty == 2:
     WINDOW = 320
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+
 def get_random_position(): return [randrange(*RANGE), randrange(*RANGE)]
+
 snake = pygame.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
 segments = [snake.copy()]
 snake_dir = (0, 0)
-time, time_step = 0, 1000
-food = snake.copy()
-food.center = get_random_position()
+time, time_step = 0, 110
+
+# Load image and create rect for it
+food_image = pygame.image.load("s.png")
+food_rect = food_image.get_rect()
+food_rect.center = get_random_position()
+
 screen = pygame.display.set_mode([WINDOW] * 2)
 clock = pygame.time.Clock()
 while True:
@@ -35,13 +42,13 @@ while True:
     screen.fill('black')
     self_eating = pygame.Rect.collidelist(snake, segments[:-1]) != -1
     if snake.left < 0 or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW or self_eating:
-        snake.center, food.center = get_random_position(), get_random_position()
+        snake.center, food_rect.center = get_random_position(), get_random_position()
         length, snake_dir = 1, (0, 0)
         segments = [snake.copy()]
-    if snake.center == food.center:
-        food.center = get_random_position()
+    if snake.center == food_rect.center:
+        food_rect.center = get_random_position()
         length += 1
-    pygame.draw.rect(screen, 'red', food)
+    screen.blit(food_image, food_rect)
     [pygame.draw.rect(screen, 'green', segment) for segment in segments]
     time_now = pygame.time.get_ticks()
     if time_now - time > time_step:
